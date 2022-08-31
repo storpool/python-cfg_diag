c1c6fba2b96edce6c97541850faed04b05492c83
Peter Pentchev <pp@storpool.com>
2022-08-31 23:44:12 +0300

    Add the year 2022 to the copyright notices

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# Copyright (c) 2020, 2021  StorPool
+# Copyright (c) 2021, 2022  StorPool
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
